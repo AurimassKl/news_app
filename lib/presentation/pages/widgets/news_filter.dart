f6f93333ff0dceb5d98be7dde3af3b42bd373f78
AurimassKl <kliokysaurimas@gmail.com>
2025-08-29 23:42:28 +0300

    Integrated source correct selection

--- lib/presentation/pages/widgets/news_filter.dart
+++ lib/presentation/pages/widgets/news_filter.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:newsapp/core/adaptive_screen.dart';
 import 'package:newsapp/domain/entities/news_source_filter.dart';
+import 'package:newsapp/domain/entities/news_sources.dart';
 import 'package:newsapp/presentation/bloc/news_articles/news_articles_bloc.dart';
 import 'package:newsapp/presentation/bloc/news_articles/news_articles_event.dart';
 import 'package:newsapp/presentation/bloc/news_articles/news_articles_state.dart';
 import 'package:newsapp/presentation/bloc/news_source/news_sources_bloc.dart';
 import 'package:newsapp/presentation/bloc/news_source/news_sources_event.dart';
+import 'package:newsapp/presentation/bloc/news_source/news_sources_state.dart';
 import 'package:sealed_countries/sealed_countries.dart';
 
 class NewsFilterBar extends StatelessWidget {
@@ -28,8 +30,8 @@ class NewsFilterBar extends StatelessWidget {
 
     return BlocBuilder<NewsArticlesBloc, NewsArticlesState>(
       builder: (context, state) {
-        final bloc = context.read<NewsArticlesBloc>();
-        final currentFilter = bloc.currentFilter;
+        final articlesBloc = context.read<NewsArticlesBloc>();
+        final currentFilter = articlesBloc.currentFilter;
 
         return SingleChildScrollView(
           scrollDirection: Axis.horizontal,
@@ -37,33 +39,84 @@ class NewsFilterBar extends StatelessWidget {
             children: [
               DropdownButton<String>(
                 value: currentFilter.country,
-                items: WorldCountry.list.map((c) {
-                  return DropdownMenuItem<String>(
-                    value: c.codeShort.toLowerCase(),
-                    child: Text("${c.emoji} ${c.name.common}"),
-                  );
-                }).toList(),
+                items: [
+                  const DropdownMenuItem<String>(
+                    value: "",
+                    child: Text("All"),
+                  ),
+                  ...WorldCountry.list.map((c) {
+                    return DropdownMenuItem<String>(
+                      value: c.codeShort.toLowerCase(),
+                      child: Text("${c.emoji} ${c.name.common}"),
+                    );
+                  }).toList()
+                ],
                 onChanged: (val) {
                   if (val == null) return;
 
-                  final newFilter = currentFilter.copyWith(country: val);
-                  bloc.add(FetchNewsArticles(newFilter));
+                  final newFilter = currentFilter.copyWith(
+                    country: val,
+                    sources: "",
+                  );
+                  articlesBloc.add(FetchNewsArticles(newFilter));
                   context.read<NewsSourcesBloc>().add(FetchNewsSources(NewsSourceFilter(country: val)));
                 },
               ),
               DropdownButton<String>(
                 value: currentFilter.category,
                 hint: const Text("Category"),
-                items: categories.map((cat) {
-                  return DropdownMenuItem<String>(
-                    value: cat,
-                    child: Text(cat[0].toUpperCase() + cat.substring(1)),
-                  );
-                }).toList(),
+                items: [
+                  const DropdownMenuItem<String>(
+                    value: "",
+                    child: Text("Category"),
+                  ),
+                  ...categories.map(
+                    (cat) {
+                      return DropdownMenuItem<String>(
+                        value: cat,
+                        child: Text(cat),
+                      );
+                    },
+                  )
+                ],
                 onChanged: (val) {
                   if (val == null) return;
-                  final newFilter = currentFilter.copyWith(category: val);
-                  bloc.add(FetchNewsArticles(newFilter));
+                  final newFilter = currentFilter.copyWith(
+                    category: val,
+                    sources: "",
+                  );
+                  articlesBloc.add(FetchNewsArticles(newFilter));
+                },
+              ),
+              BlocBuilder<NewsSourcesBloc, NewsSourcesState>(
+                builder: (context, sourcesState) {
+                  return DropdownButton<String>(
+                    value: currentFilter.sources,
+                    hint: const Text("Source"),
+                    items: [
+                      const DropdownMenuItem<String>(
+                        value: "",
+                        child: Text("Source"),
+                      ),
+                      ...(sourcesState is NewsSourcesFetchedState ? sourcesState.newsSources : <NewsSource>[]).map(
+                        (src) {
+                          return DropdownMenuItem<String>(
+                            value: src.id,
+                            child: Text(src.name),
+                          );
+                        },
+                      ).toList()
+                    ],
+                    onChanged: (val) {
+                      if (val == null) return;
+                      final newFilter = currentFilter.copyWith(
+                        sources: val,
+                        country: "",
+                        category: "",
+                      );
+                      articlesBloc.add(FetchNewsArticles(newFilter));
+                    },
+                  );
                 },
               ),
             ],
